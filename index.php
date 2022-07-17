673b75b98a4a9d4a59556de1bc52d4b6dabc26fa
MansuriyaBrij <brijmansuriya.ai@gmail.com>
2022-07-17 16:17:08 +0530

    2
    2

--- index.php
+++ index.php
@@ -6,5 +6,6 @@ $headers = 'From: webmaster@example.com' . "\r\n" .
     'Reply-To: webmaster@example.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $message, $headers);
+$test = mail($to, $subject, $message, $headers);
+print_r($test);
 ?>
